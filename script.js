bc7186694921c7fccc3e8c8998ee2b30e54c712d
fedegarcia2008 <garcia.federico@tecnica7.edu.ar>
2026-06-19 21:04:09 -0300

    LLamar json empleados

--- script.js
+++ script.js
@@ -7,7 +7,22 @@ fetch('pasajero.json')
     datos.forEach(pas => {
         salida += `<p>Nombre: ${pas.nombres}</p>`;
     });
-    document.getElementById("resultado").innerHTML = salida;
+    document.getElementById("result_pas").innerHTML = salida;
+})
+.catch((e) => {
+    alert(`Error al leer \n${e}`);
+});
+
+fetch('empleado.json')
+.then((rpta) => {
+    return rpta.json();
+})
+.then((datos) => {
+    let salida = "";
+    datos.forEach(emp => {
+        salida += `<p>Nombre: ${emp.nombres}</p>`;
+    });
+    document.getElementById("result_emp").innerHTML = salida;
 })
 .catch((e) => {
     alert(`Error al leer \n${e}`);
